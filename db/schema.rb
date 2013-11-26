22ad9c76d3cd518f165b0f8119514550bd9b8227
Clee681 <christopher.lee681@gmail.com>
2013-11-26 16:55:01 -0500

    Add correct_answer column to questions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131126202144) do
+ActiveRecord::Schema.define(version: 20131126215402) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 20131126202144) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "sequence"
+    t.string   "correct_answer"
   end
 
   add_index "questions", ["assignment_id"], name: "index_questions_on_assignment_id", using: :btree
