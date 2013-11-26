b2dd262a22d83c48a1b0f8c44ea29a8be5d57e57
Clee681 <christopher.lee681@gmail.com>
2013-11-26 16:39:33 -0500

    Spencer refactoring

--- app/models/student.rb
+++ app/models/student.rb
@@ -11,27 +11,39 @@ class Student < ActiveRecord::Base
   has_many :answers
   has_many :messages
 
+  def send_next_question!
+    if self.next_question
+      Message.create(
+        from: self.next_question.assignment.teacher.phone_number,
+        to: self.phone_number,
+        content: self.next_question.content,
+        assignment: self.next_question.assignment,
+        student: self
+      ).send_text_message
+
+      self.update(current_question: self.next_question.id)
+    else
+      self.update(current_question: nil)
+    end
+  end
+
+  def record_answer(answer)
+    question = Question.find(student.current_question)
+    self.answers.create(question: question, response: answer)
+  end
+
   def next_question
     assignment = Assignment.find(self.current_assignment)
     questions = assignment.questions.order("sequence ASC")
 
     if self.current_question == nil
       # current question is #1
-      self.current_question = questions.first.id
-      self.save
       questions.first
     else
       # get next question based on question order and the current_question
       question = Question.find(self.current_question)
       new_index = questions.index(question).next
 
-      if questions[new_index].nil?
-        self.current_question = nil
-      else
-        self.current_question = questions[new_index].id
-      end
-      
-      self.save
       questions[new_index]
     end
   end
