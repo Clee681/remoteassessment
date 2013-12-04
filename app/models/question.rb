1ec9d0195cf7ee2437f60089dece7eedabfffee8
Theo Vora <theevo@gmail.com>
2013-12-04 18:51:41 -0500

    create progress bars for each answered question in assignment#show

--- app/models/question.rb
+++ app/models/question.rb
@@ -6,4 +6,24 @@ class Question < ActiveRecord::Base
   validates_presence_of :content, :correct_answer
   validates_length_of :content, within: 1..160
   validates_length_of :correct_answer, within: 1..160
+
+  def total_answers
+    answers.count.to_f
+  end
+
+  def correct_answers
+    answers.where(correct: true).count
+  end
+
+  def wrong_answers
+    answers.where(correct: false).count
+  end
+
+  def percent_correct
+    (correct_answers/total_answers*100).to_i
+  end
+
+  def percent_wrong
+    100-percent_correct
+  end
 end
